cf444b1fa6de61f0611190d831c657ede205dfee
RizakSingh <srizak95@gmail.com>
2025-12-27 00:52:24 +0530

    add temporary admin register route

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
 
 const router = express.Router();
-const { login } = require("../controllers/authController");
+const { login,registerAdmin } = require("../controllers/authController");
 
 router.post("/login", login);
+router.post("/register-admin", registerAdmin);
 
 module.exports = router;
